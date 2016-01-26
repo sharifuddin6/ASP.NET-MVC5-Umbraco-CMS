8d559fae703d5c31361da557d6855d4e851ac679
Sharif Uddin <sharifuddin6@outlook.com>
2016-01-26 23:56:41 +0000

    Added bootstrap with theme and updated jquery framework.

---
+++ MVC5-Umbraco-CMS/MVC5-Umbraco-CMS/App_Start/UmbracoEventHandlers.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+using Umbraco.Core;
+
+namespace MVC5_Umbraco_CMS
+{
+    public class UmbracoEventHandlers : ApplicationEventHandler
+    {
+        protected override void ApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            BundleConfig.RegisterBundles(BundleTable.Bundles);    
+        } 
+    }
+}
